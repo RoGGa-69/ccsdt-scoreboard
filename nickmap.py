8efad9b54070649d5780a6ceafb603aadc4e680f
Edgar Bering <ebering@csclub.uwaterloo.ca>
2018-10-05 20:35:37 -0400

    nickmap generator
    (cherry picked from commit a8fd9d7c32918866a6cf8812ec7e9f51d78290c2)

---
+++ nickmap.py
@@ -0,0 +1,23 @@
+import csdc
+import orm
+import model
+import yaml
+import os
+
+CONFIG_FILE = 'config.yml'
+if not os.path.isfile(CONFIG_FILE):
+    CONFIG_FILE = 'config_default.yml'
+
+CONFIG = yaml.safe_load(open(CONFIG_FILE, encoding='utf8'))
+
+orm.initialize(CONFIG['db uri'])
+model.setup_database()
+print("!nick -rm csdc22")
+with orm.get_session() as s:
+    buf = "";
+    for p in s.query(orm.CsdcContestant).all():
+        if len(buf + " " + p.player.name) > 275:
+            print("!nick csdc22 " + buf)
+            buf = p.player.name
+        else: 
+            buf = buf + " " + p.player.name
